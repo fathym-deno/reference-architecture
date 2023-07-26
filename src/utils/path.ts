d0bf1a0a26a68885d39c9efe306ebe390e5a3306
mcgear <michael.gearhardt@gmail.com>
2023-07-26 09:20:24 -0600

    Docs for exists

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -1,3 +1,8 @@
+/**
+ * Determine if a path exists.
+ * @param path The path to check for existence.
+ * @returns If the path exists.
+ */
 export async function exists(path: string): Promise<boolean> {
   try {
     await Deno.stat(path);
