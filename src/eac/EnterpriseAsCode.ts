4700441cf7185c202fcf07147849224b33b169e3
michael.gearhardt@fathym.com <michael.gearhardt@fathym.com>
2023-08-16 07:13:02 +0000

    "Update EnterpriseAsCode and create new interface files for improved compatibility and organization."

--- src/eac/EnterpriseAsCode.ts
+++ src/eac/EnterpriseAsCode.ts
@@ -1,5 +1,26 @@
 import { EaCDetails } from './EaCDetails';
+import { EaCAccessRightAsCode } from './EaCAccessRightAsCode';
+import { EaCApplicationAsCode } from './EaCApplicationAsCode';
+import { EaCDataTokenAsCode } from './EaCDataTokenAsCode';
+import { EaCEnvironmentAsCode } from './EaCEnvironmentAsCode';
+import { EaCHostAsCode } from './EaCHostAsCode';
+import { EaCLicenseConfigurationAsCode } from './EaCLicenseConfigurationAsCode';
+import { EaCDFSModifierAsCode } from './EaCDFSModifierAsCode';
+import { EaCProjectAsCode } from './EaCProjectAsCode';
+import { EaCProviderAsCode } from './EaCProviderAsCode';
 
 export interface EnterpriseAsCode extends EaCDetails {
-  // Updated to match the provided C# file
+  AccessRights?: { [key: string]: EaCAccessRightAsCode };
+  Applications?: { [key: string]: EaCApplicationAsCode };
+  DataTokens?: { [key: string]: EaCDataTokenAsCode };
+  EnterpriseLookup?: string;
+  Environments?: { [key: string]: EaCEnvironmentAsCode };
+  Hosts?: { [key: string]: EaCHostAsCode };
+  LicenseConfigurations?: { [key: string]: EaCLicenseConfigurationAsCode };
+  Modifiers?: { [key: string]: EaCDFSModifierAsCode };
+  ParentEnterpriseLookup?: string;
+  PrimaryEnvironment?: string;
+  PrimaryHost?: string;
+  Projects?: { [key: string]: EaCProjectAsCode };
+  Providers?: { [key: string]: EaCProviderAsCode };
 }
