c283e34d176c16c9d8bb8b8412836a4bf37e495b
michael.gearhardt@fathym.com <michael.gearhardt@fathym.com>
2023-08-16 19:02:16 +0000

    Update interface to match new specification

--- src/eac/EaCCloudAsCode.ts
+++ src/eac/EaCCloudAsCode.ts
@@ -1,1 +1,10 @@
-import { EaCCloudResourceGroupAsCode } from "./EaCCloudResourceGroupAsCode.ts";
+import { EaCCloudDetails } from "./EaCCloudDetails";
+import { EaCDetails } from "./EaCDetails";
+import { EaCLandingZoneAsCode } from "./EaCLandingZoneAsCode";
+import { EaCCloudResourceGroupAsCode } from "./EaCCloudResourceGroupAsCode";
+
+export interface EaCCloudAsCode extends EaCDetails<EaCCloudDetails> {
+  LandingZones?: Record<string, EaCLandingZoneAsCode>;
+  ResourceGroups?: Record<string, EaCCloudResourceGroupAsCode>;
+  Type?: string;
+}
