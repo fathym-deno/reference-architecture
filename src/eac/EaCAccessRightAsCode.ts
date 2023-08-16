257392d11769c3c13778e649b27285cd36859de0
michael.gearhardt@fathym.com <michael.gearhardt@fathym.com>
2023-08-16 16:08:22 +0000

    Update import statements, add interface, and refactor code structure.

--- src/eac/EaCAccessRightAsCode.ts
+++ src/eac/EaCAccessRightAsCode.ts
@@ -1,1 +1,6 @@
-export interface EaCAccessRightAsCode extends EaCDetails<EaCAccessRightDetails> {  }
+import { EaCDetails } from "./EaCDetails.ts";
+import { EaCAccessRightDetails } from "./EaCAccessRightDetails.ts";
+
+export interface EaCAccessRightAsCode extends EaCDetails<EaCAccessRightDetails> {
+  // Add any additional properties or methods here
+}
---
+++ src/eac/EaCAccessRightDetails.ts
@@ -0,0 +1,5 @@
+import { EaCVertexDetails } from './EaCVertexDetails.ts';
+
+export interface EaCAccessRightDetails extends EaCVertexDetails {
+  // Add any additional properties or methods here
+}
