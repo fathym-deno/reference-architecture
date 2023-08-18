0e274ac6ab331797b0021c5d620b13cea2f7d8dc
mcgear <michael.gearhardt@gmail.com>
2023-08-18 00:22:44 -0600

    Base ReqRes

--- src/services/_exports.ts
+++ src/services/_exports.ts
@@ -1,3 +1,5 @@
+export * from "./BaseRequest.ts";
+export * from "./BaseResponse.ts";
 export * from "./ObservableContextService.ts";
 export * from "./RealTimeConnection.ts";
 export * from "./StateContext.ts";
