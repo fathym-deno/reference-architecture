a03883e6d058e420ffe33a4b7f6e93ce277d521f
mcgear <michael.gearhardt@gmail.com>
2023-08-18 00:21:06 -0600

    Base ReqRes

---
+++ src/services/BaseRequest.ts
@@ -0,0 +1,1 @@
+export type BaseRequest = Record<string | number | symbol, unknown>;
---
+++ src/services/BaseResponse.ts
@@ -0,0 +1,9 @@
+import { Status } from "../status.ts";
+
+export type BaseResponse = Record<string | number | symbol, unknown> & {
+  Status: Status;
+};
+
+export type BaseResponseModel<TModel> = BaseResponse & {
+  Model: TModel;
+};
