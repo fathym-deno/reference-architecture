8ccce0be6ae65e52e1c1c3222d26a8b75d89b2f5
mcgear <michael.gearhardt@gmail.com>
2023-11-10 20:41:24 -0700

    change to merge library

--- src/utils/object.helpers.ts
+++ src/utils/object.helpers.ts
@@ -1,22 +1,24 @@
-// deno-lint-ignore-file no-explicit-any
-export function merge(
-  input: Record<string | number | symbol, unknown>,
-  update: Record<string | number | symbol, unknown>,
-): Record<string | number | symbol, unknown> {
-  return Object.entries(input).reduce((acc, [key, value]) => {
-    if (!(key in update)) return { ...acc, [key]: value };
+// // deno-lint-ignore-file no-explicit-any
+// export function merge(
+//   input: Record<string | number | symbol, unknown>,
+//   update: Record<string | number | symbol, unknown>,
+// ): Record<string | number | symbol, unknown> {
+//   return Object.entries(input).reduce((acc, [key, value]) => {
+//     if (!(key in update)) return { ...acc, [key]: value };
 
-    // TODO: How to handle arrays?  Currently overwrites with new array values
+//     // TODO: How to handle arrays?  Currently overwrites with new array values
 
-    if (
-      typeof update[key] === "object" &&
-      update[key] !== null &&
-      update[key] !== undefined &&
-      !Array.isArray(update[key])
-    ) {
-      return { ...acc, [key]: merge((input as any)[key], update[key] as any) };
-    } else {
-      return { ...acc, [key]: update[key] };
-    }
-  }, {});
-}
+//     if (
+//       typeof update[key] === "object" &&
+//       update[key] !== null &&
+//       update[key] !== undefined &&
+//       !Array.isArray(update[key])
+//     ) {
+//       return { ...acc, [key]: merge((input as any)[key], update[key] as any) };
+//     } else {
+//       return { ...acc, [key]: update[key] };
+//     }
+//   }, {});
+// }
+
+export * from "npm:ts-deepmerge";
