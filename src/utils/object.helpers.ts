fd53b4d651ae6f9817b878ec628f57cbd081aa6f
mcgear <michael.gearhardt@gmail.com>
2024-01-10 08:24:27 -0700

    deep merge with deno native

--- src/utils/object.helpers.ts
+++ src/utils/object.helpers.ts
@@ -1,23 +1,17 @@
-import deepmerge from "https://esm.sh/ts-deepmerge@6.2.0";
+import { deepMerge } from "$std/collections/mod.ts";
 
-export function merge<T>(
-  ...inputs: object[]
-): T {
-  return deepmerge.withOptions(
-    {
-      mergeArrays: false,
-    },
-    ...inputs,
-  ) as T;
+export function merge<T>(...inputs: object[]): T {
+  return inputs.reduce((prev, cur) => {
+    return deepMerge(prev, cur, {
+      arrays: "replace",
+      maps: "replace",
+      sets: "replace",
+    });
+  }, {}) as T;
 }
 
-export function mergeWithArrays<T>(
-  ...inputs: object[]
-): T {
-  return deepmerge.withOptions(
-    {
-      mergeArrays: true,
-    },
-    ...inputs,
-  ) as T;
+export function mergeWithArrays<T>(...inputs: object[]): T {
+  return inputs.reduce((prev, cur) => {
+    return deepMerge(prev, cur);
+  }, {}) as T;
 }
