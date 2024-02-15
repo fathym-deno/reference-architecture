91a5106fb87fd896f9756108a00313d8ca157626
mcgear <michael.gearhardt@gmail.com>
2024-02-15 10:10:50 -0700

    Rework exports

--- mod.ts
+++ mod.ts
@@ -1,5 +1,1 @@
-// export * from "./src/eac/_exports.ts";
-export * from "./src/services/_exports.ts";
-export * from "./src/utils/_exports.ts";
-export * from "./src/status.ts";
-export * from "./src/iterables/_exports.ts";
+export * from "./src/_exports.ts";
---
+++ src/_exports.ts
@@ -0,0 +1,5 @@
+// export * from "./eac/_exports.ts";
+export * from "./services/_exports.ts";
+export * from "./utils/_exports.ts";
+export * from "./status.ts";
+export * from "./iterables/_exports.ts";
