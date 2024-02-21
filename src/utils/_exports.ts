d57bef24b17d698874d1da15009fff9812c284cf
mcgear <michael.gearhardt@gmail.com>
2024-02-21 09:00:24 -0700

    exists change

--- src/utils/_exports.ts
+++ src/utils/_exports.ts
@@ -2,4 +2,5 @@ export * from "./http.helpers.ts";
 export * from "./iterables.ts";
 export * from "./object.helpers.ts";
 export * from "./path.ts";
+export * from "./path/exists.ts";
 export * from "./tailwind.helpers.ts";
