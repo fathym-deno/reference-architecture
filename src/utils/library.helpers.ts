dc83c1f1145b81a99d0923d0e7d49284fb204344
mcgear <michael.gearhardt@gmail.com>
2024-02-25 16:14:52 -0700

    method adjustments

--- src/utils/library.helpers.ts
+++ src/utils/library.helpers.ts
@@ -1,3 +1,10 @@
-export function loadMetaUrl(path: string): string {
-  return import.meta.resolve(path);
+export function loadMetaUrl(
+  resolve: (specifier: string) => string,
+  path: string,
+): string {
+  return resolve(path);
+}
+
+export function loadRefArchMetaUrl(path: string): string {
+  return loadMetaUrl(import.meta.resolve, path);
 }
