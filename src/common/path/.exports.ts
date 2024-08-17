c682bcc3a536609305d518291f16d39d6bfc0d98
mcgear <michael.gearhardt@gmail.com>
2024-08-17 16:32:10 -0600

    try with examples

--- src/common/path/.exports.ts
+++ src/common/path/.exports.ts
@@ -1,30 +1,24 @@
-// /**
-//  * @module
-//  *
-//  * A series of helper functions to assist with file paths and directories.
-//  *
-//  * @example
-//  * ```typescript
-//  * import { exists } from '@fathym/common/path';
-//  *
-//  * if (await exists('/path/to/file.txt')) {
-//  *  console.log('File exists');
-//  * }
-//  * ```
-//  *
-//  * @example
-//  * ```typescript
-//  * import { existsSync } from '@fathym/common';
-//  *
-//  * if (existsSync('/path/to/file.txt')) {
-//  *  console.log('File exists');
-//  * }
-//  * ```
-//  */
 /**
- * Lower level `transform` functionality that's used by the CLI
- * to convert Deno code to Node code.
+//  * A series of helper functions to assist with file paths and directories.
  * @module
+ *
+ * @example
+ * ```typescript
+ * import { exists } from '@fathym/common/path';
+ *
+ * if (await exists('/path/to/file.txt')) {
+ *  console.log('File exists');
+ * }
+ * ```
+ *
+ * @example
+ * ```typescript
+ * import { existsSync } from '@fathym/common';
+ *
+ * if (existsSync('/path/to/file.txt')) {
+ *  console.log('File exists');
+ * }
+ * ```
  */
 
 export * from './exists.ts';
