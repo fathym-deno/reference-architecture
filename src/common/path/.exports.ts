58acf21ea52855d89c31be3bef80116a2f86bb15
mcgear <michael.gearhardt@gmail.com>
2024-08-17 16:28:33 -0600

    try this

--- src/common/path/.exports.ts
+++ src/common/path/.exports.ts
@@ -1,25 +1,30 @@
-/**
- * @module
- *
- * A series of helper functions to assist with file paths and directories.
- *
- * @example
- * ```typescript
- * import { exists } from '@fathym/common/path';
- *
- * if (await exists('/path/to/file.txt')) {
- *  console.log('File exists');
- * }
- * ```
- *
- * @example
- * ```typescript
- * import { existsSync } from '@fathym/common';
- *
- * if (existsSync('/path/to/file.txt')) {
- *  console.log('File exists');
- * }
- * ```
- */
+// /**
+//  * @module
+//  *
+//  * A series of helper functions to assist with file paths and directories.
+//  *
+//  * @example
+//  * ```typescript
+//  * import { exists } from '@fathym/common/path';
+//  *
+//  * if (await exists('/path/to/file.txt')) {
+//  *  console.log('File exists');
+//  * }
+//  * ```
+//  *
+//  * @example
+//  * ```typescript
+//  * import { existsSync } from '@fathym/common';
+//  *
+//  * if (existsSync('/path/to/file.txt')) {
+//  *  console.log('File exists');
+//  * }
+//  * ```
+//  */
 
-export * from './exists.ts';
+// export * from './exists.ts';
+/**
+ * Lower level `transform` functionality that's used by the CLI
+ * to convert Deno code to Node code.
+ * @module
+ */
