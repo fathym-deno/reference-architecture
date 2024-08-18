1022397fa37d60f1e93fda3601b55e2d7c20a54a
Michael Gearhardt <michael.gearhardt@fathym.com>
2024-08-17 20:03:03 -0600

    docs

--- src/utils/iterables.ts
+++ src/utils/iterables.ts
@@ -1,3 +1,13 @@
+/**
+ * Convert an async iterable to an async iterable of another type.
+ * 
+ * @source The incoming iterable to convert
+ * @converter The method to use for converting the item.
+ * @return The async iterable of converted items.
+ * 
+ * @example From direct import
+ * 
+ */
 export async function* convertAsyncIterable<T, R>(
   source: AsyncIterable<T>,
   converter: (item: T) => Promise<R>,
