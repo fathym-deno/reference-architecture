1db2acc189213441443c890cc8f3eadb600ba2a9
mcgear <michael.gearhardt@gmail.com>
2024-08-18 18:52:17 -0600

    More oauth methods

--- src/oauth/.exports.ts
+++ src/oauth/.exports.ts
@@ -2,10 +2,13 @@
  * Helpers to support working OAuth in Deno with DenoKV.
  * @module
  */
-export * from "./OAuthHelpers.ts";
-export * from "./creatAzureADB2COAuthConfig.ts";
-export * from "./creatOAuthConfig.ts";
-export * from "./createAzureADOAuthConfig.ts";
-export * from "./createGitHubOAuthConfig.ts";
-export * from "./createOAuthHelpers.ts";
-export * from "./oAuthRequest.ts";
+export * from './creatAzureADB2COAuthConfig.ts';
+export * from './creatAzureADB2COAuthConfig.ts';
+export * from './createAzureADOAuth.ts';
+export * from './createAzureADOAuthConfig.ts';
+export * from './createGitHubOAuth.ts';
+export * from './createGitHubOAuthConfig.ts';
+export * from './createOAuthHelpers.ts';
+export * from './creatOAuthConfig.ts';
+export * from './OAuthHelpers.ts';
+export * from './oAuthRequest.ts';
