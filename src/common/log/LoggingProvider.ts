ef0fe4856f8f9075ade48d5a9a1dc36dd8e68211
mcgear <michael.gearhardt@gmail.com>
2024-08-26 22:16:23 -0600

    logging interface updates

--- src/common/log/LoggingProvider.ts
+++ src/common/log/LoggingProvider.ts
@@ -1,11 +1,16 @@
 import { getLogger, type LogConfig, type Logger, setup } from "./.deps.ts";
+import { getPackageLogger, getPackageLoggerSync } from "./getPackageLogger.ts";
 
 export class LoggingProvider {
   constructor(protected setupConfig: LogConfig) {
     setup(setupConfig);
   }
 
-  public Logger(name?: string): Logger {
-    return getLogger(name);
+  public async Logger(name?: string, usePackage?: boolean): Promise<Logger> {
+    return usePackage ? await getPackageLogger(name) : getLogger(name);
+  }
+
+  public LoggerSync(name?: string, usePackage?: boolean): Logger {
+    return usePackage ? getPackageLoggerSync(name) : getLogger(name);
   }
 }
