cc2ac619d569e0857927cdb7eb7eb232ed6e0995
mcgear <michael.gearhardt@gmail.com>
2024-09-01 02:14:10 -0600

    fix is object

--- src/common/types/IsObject.ts
+++ src/common/types/IsObject.ts
@@ -3,6 +3,7 @@
 /**
  * Provides a type to check if a given type is an object that is not an array.
  */
-export type IsObject<T> = T extends object ? T extends any[] ? false
+export type IsObject<T> = NonNullable<T> extends object
+  ? NonNullable<T> extends any[] ? false
   : true
   : false;
