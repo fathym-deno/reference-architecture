152905fc754eceb7a8c18dd509f98de9cd6a28a2
mcgear <michael.gearhardt@gmail.com>
2024-09-01 22:53:05 -0600

    Fix IsNotUndefined

--- src/common/types/IsNotUndefined.ts
+++ src/common/types/IsNotUndefined.ts
@@ -1,9 +1,6 @@
+import type { IsUndefined } from "./IsUndefined.ts";
+
 /**
  * Used to determine if a type is not undefined or optionally undefined.
  */
-
-export type IsNotUndefined<T> = [T] extends [undefined] ? true
-  : [undefined] extends [T] ? true
-  : T extends undefined ? true
-  : undefined extends T ? true
-  : false;
+export type IsNotUndefined<T> = IsUndefined<T> extends true ? false : true;
