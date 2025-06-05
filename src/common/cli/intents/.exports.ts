4747a377020fd03700f7a313889296a8847e8a01
mcgear <michael.gearhardt@gmail.com>
2025-06-05 15:48:29 -0600

    checkpoint

--- src/common/cli/intents/.exports.ts
+++ src/common/cli/intents/.exports.ts
@@ -1,3 +1,5 @@
 export * from './CommandIntent.ts';
+export * from './CommandIntents.ts';
 export * from './CommandIntentBuilder.ts';
 export * from './CommandIntentRuntime.ts';
+export * from './CommandIntentsBuilder.ts';
